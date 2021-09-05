909964b56b4e5541433a0c562732c5064feae6a9
Klinsmann-Agyei <87317341+Klinsmann-Agyei@users.noreply.github.com>
2021-09-05 17:22:47 +0000

    Dog Years Project

---
+++ dogyears.js
@@ -0,0 +1,13 @@
+var myAge = 25; // My Age
+var earlyYears =2; // Early Years
+earlyYears *= 10.5;
+var laterYears = myAge - 2; // later years
+laterYears *= 4; // Later years multiplied by 4
+myAgeInDogYears = earlyYears + laterYears;// adding early years to later years
+var myName = "Klinsmann"
+myName = myName.toLowerCase(); // Changing my name to lower case
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+console.log(myName)
+console.log(laterYears)
